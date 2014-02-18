d41d89bcaf657abfc3e894c1a1a27173cce74b39
David Jedeikin <djedeikin@sharethis.com>
2014-02-18 11:04:52 -0800

    A few missed initial files

---
+++ SocializeIntegTests/SocializeIntegTests.m
@@ -0,0 +1,34 @@
+//
+//  SocializeIntegTests.m
+//  SocializeIntegTests
+//
+//  Created by David Jedeikin on 2/18/14.
+//  Copyright (c) 2014 ShareThis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SocializeIntegTests : XCTestCase
+
+@end
+
+@implementation SocializeIntegTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
