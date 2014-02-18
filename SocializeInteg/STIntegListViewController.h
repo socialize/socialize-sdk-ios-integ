a8cddc4a57f2fd229e3eb81af3558a1ae1de6efe
David Jedeikin <djedeikin@sharethis.com>
2014-02-18 11:56:20 -0800

    Initial commit with Pods

---
+++ SocializeInteg/STIntegListViewController.h
@@ -0,0 +1,13 @@
+//
+//  STIntegListViewController.h
+//  SocializeInteg
+//
+//  Created by David Jedeikin on 2/18/14.
+//  Copyright (c) 2014 ShareThis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STIntegListViewController : UITableViewController
+
+@end
