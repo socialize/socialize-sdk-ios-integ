c95d6a38f536c48850459c7f26ce320a0b3d0e76
David Jedeikin <djedeikin@sharethis.com>
2014-02-18 10:59:25 -0800

    Initial commit of empty project

---
+++ SocializeInteg/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  SocializeInteg
+//
+//  Created by David Jedeikin on 2/18/14.
+//  Copyright (c) 2014 ShareThis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SocializeInteg/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SocializeInteg
+//
+//  Created by David Jedeikin on 2/18/14.
+//  Copyright (c) 2014 ShareThis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
